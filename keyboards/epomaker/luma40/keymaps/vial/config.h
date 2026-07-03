216fbbd3a4250951bca4452fa417fca782864341
sfabris <simone.fabris@gmail.com>
2026-07-03 09:29:04 +0200

    Add Vial keymap for Epomaker Luma40
    VialRGB lighting, QMK Settings, 16 combo/key-override/alt-repeat
    slots (tap dance left out to keep flash/EEPROM in budget), ~770 bytes
    of macro space. Unlock combo: Tab + Backspace. The customKeycodes
    order in vial.json follows the Custom_Keycodes enum in
    rdmctmzt_common.h (positional mapping from QK_KB).

---
+++ keyboards/epomaker/luma40/keymaps/vial/config.h
@@ -0,0 +1,27 @@
+/* Copyright 2026 Simone Fabris
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB3, 0x52, 0xF8, 0xA7, 0x49, 0x3B, 0x36, 0xD0}
+
+/* Unlock combo: Tab (0,0) + Backspace (0,11) held together */
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+
+/* Con EEPROM_SIZE 2048 i tier automatici di vial.h assegnano 16 entry a
+ * combo/key override/alt-repeat; lo spazio EEPROM residuo (~770 byte) va
+ * alle macro. */
---
+++ keyboards/epomaker/luma40/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+
+# Tap dance disattivato per scelta (EEPROM emulata da 2048 byte, si
+# privilegiano QMK Settings, Combo, Key Override e Alt-Repeat; le macro
+# ricevono lo spazio EEPROM residuo, ~770 byte).
+TAP_DANCE_ENABLE = no
